843da3db74db0f2a260dd08a1b1ba23572e54a53
ViciousVilicus <103430000+ViciousVilicus@users.noreply.github.com>
2022-05-07 23:17:57 +0200

    Separation of Creatures, Implementation of Predator, Plant
    Separation of Creatures into Pray, Predator, Plant 
    Implementation of Predator, Plant and the ability to consume other creatures (or "plants")
    Not yet runnable

--- LifeSim.py
+++ LifeSim.py
@@ -1,20 +1,21 @@
 import random
 import time
 
-from class1 import Creature
+from class1 import Creature, Pray, Predator, Plant
 
 # Proper clear doesn't want to work, used the 'print a lot of lines' solution:
 # Works but meh, on a bigger map it will be better
 # Random doesn't want to work, no idea what is wrong, it worked on the monopoly one:
 # My dumb ass was adding onto the list, but displaying only the first generation
 # so great, made my first memory leak...
-MapDimensions = 10
+MapDimensions = 3
 Map = [[None for col in range(MapDimensions)] for row in range(MapDimensions)]
 CreatureList = []
 # CURRENT   Implement new features - predators eating prey, prey eating plants, plants randomly spawning
 # PRIORITY  or rework some old stuff
 # Potential methods - divide predators and prey into subclasses of Creature
 
+# FIXED
 # BUG Found - Creatures have always very specific coordinates at the beginning of the simulation
 # reconsider first map generation - picking creatures coordinates might be better to be done elsewhere
 # what - GenerateFirstMapGeneration - does this even do anything
@@ -35,10 +36,73 @@ def GenerateFirstMapGeneration():
         Map = [[None for col in range(MapDimensions)] for row in range(MapDimensions)]
         # this may not be necessary if I will make this solely for the first map generation
 
+
+def UpdateMap():
+    global Map
+
+    Map.clear()
+    Map = [[None for col in range(MapDimensions)] for row in range(MapDimensions)]
+
+    for i in range(CreatureList.__len__()):
+        # No checks are needed here, they are made by the creature
+        Map[CreatureList[i].position_x][CreatureList[i].position_y] = CreatureList[i]
+
+
+# noinspection PyUnresolvedReferences
+def DisplayMap():
+    for col in range(len(Map)):
+        print(f"{col}", end=" ")
+        for row in range(len(Map[col])):
+            # Big problem/security flaw here. What if the error will be a valid one?
+            # Re-do this with a check that bools only when it is a creature type | in not None might just work
+            if Map[col][row] is not None:
+                if Map[col][row].creature_type == "Prey":
+                    print("X", end=" ")
+                elif Map[col][row].creature_type == "Predator":
+                    print("Z", end=" ")
+                elif Map[col][row].creature_type == "Plant":
+                    print("P", end=" ")
+                else:
+                    print("?", end=" ")
+            elif Map[col][row] is None:
+                print(" ", end=" ")
+            else:  # I know this is dumb
+                print("?", end=" ")
+        print()
+
+
+def GenerateCreatures(creature_id):
+    while True:
+        pray_amount = int(input("Amount of pray? "))
+        predator_amount = int(input("Amount of predators? "))
+        # safety check so the map will not be over 100% of the amount of map tiles
+        plant_amount = pray_amount  # for now 1 to 1 ratio
+        if pray_amount + predator_amount + plant_amount > MapDimensions * MapDimensions:
+            print("Too many creatures")
+            continue
+        if pray_amount > 0 or predator_amount > 0:
+            print(f"{pray_amount + predator_amount + plant_amount} Objects to be initialised")
+            break
+    GenerateCreatures.CreatureId = creature_id
+
+    for i in range(pray_amount):
+        CreatureList.append(Pray("Prey", False, GenerateCreatures.CreatureId))
+        GenerateCreatures.CreatureId += 1
+
+    for i in range(predator_amount):
+        CreatureList.append(Predator("Predator", False, GenerateCreatures.CreatureId))
+        GenerateCreatures.CreatureId += 1
+
+    for i in range(plant_amount):
+        CreatureList.append(Plant("Plant", False, GenerateCreatures.CreatureId))
+        GenerateCreatures.CreatureId += 1
+
+    global Map
+
     for i in range(CreatureList.__len__()):
         while True:
-            CreatureList[i].position_x = random.randint(0, 9)
-            CreatureList[i].position_y = random.randint(0, 9)  # do it using the creature position instead of map field
+            CreatureList[i].position_x = random.randint(0, MapDimensions-1)
+            CreatureList[i].position_y = random.randint(0, MapDimensions-1)  # do it using the creature position instead of map field
 
             # If it is something else than a None field, aka creature
             # re-roll positions, if not break and put it on the map
@@ -50,55 +114,12 @@ def GenerateFirstMapGeneration():
         Map[CreatureList[i].position_x][CreatureList[i].position_y] = CreatureList[i]
 
 
-def UpdateMap():
-    global Map
-
-    if Map.__len__ != 0:  # this is dumb
-        Map.clear()
-        Map = [[None for col in range(MapDimensions)] for row in range(MapDimensions)]
-
-    for i in range(CreatureList.__len__()):
-        # No checks are needed here, they are made by the creature
-        Map[CreatureList[i].position_x][CreatureList[i].position_y] = CreatureList[i]
-
-
-def DisplayMap():
-    for col in range(len(Map)):
-        print(f"{col}", end=" ")
-        for row in range(len(Map[col])):
-            # Big problem/security flaw here. What if the error will be a valid one?
-            # Re-do this with a check that bools only when it is a creature type | in not None might just work
-            try:
-                if Map[col][row].type == "Prey":
-                    print("X", end=" ")
-                elif Map[col][row].type == "Predator":
-                    print("Z", end=" ")
-            except:  # find out what the error actually is called - May be reference error
-                print(" ", end=" ")
-        print()
-
-
-def GenerateCreatures(creature_id):
-    while True:
-        pray_amount = int(input("Amount of pray? "))
-        predator_amount = int(input("Amount of predators? "))
-        # safety check so the map will not be over 100% of the amount of map tiles
-        if pray_amount + predator_amount > MapDimensions * MapDimensions:
-            print("Too many creatures")
-            continue
-        if pray_amount > 0 or predator_amount > 0:
-            break
-    GenerateCreatures.CreatureId = creature_id
-
-    for i in range(pray_amount):
-        CreatureList.append(Creature("Prey", False, GenerateCreatures.CreatureId))
-        GenerateCreatures.CreatureId += 1
-
-
 def CreatureActions():
     # CreatureList = sorted(CreatureList, key=lambda item: item.id)  # is this actually necessary or wanted
     for i in range(CreatureList.__len__()):
-        CreatureList[i].move(random.randint(0, 4), Map)
+        if CreatureList[i].creature_type is not "Plant":
+            CreatureList[i].move(random.randint(0, 4), Map, CreatureList)
+        # don't know if this should be included
         Map[CreatureList[i].position_x][CreatureList[i].position_y] = CreatureList[i]
 
 
@@ -112,7 +133,7 @@ def EndPrint(generation):
     print(generation)
     print(string)
 
-# how I can do this,
+# how it works
 # do a list of object creatures
 # put them on the map
 # cycle the list of creatures to find out where the specific creature is
@@ -127,11 +148,11 @@ def EndPrint(generation):
 test_iterator = 0
 CreatureIdOuter = 0
 
-GenerateFirstMapGeneration()
+# GenerateFirstMapGeneration()
 GenerateCreatures(CreatureIdOuter)  # generate creatures before going into the game loop
 while True:
     test_iterator += 1
-    CreatureIdOuter = GenerateCreatures.CreatureId  # gets out the creature id from the function
+    # CreatureIdOuter = GenerateCreatures.CreatureId  # gets out the creature id from the function
     # GenerateCreatures.CreatureId = GenerateCreatures.CreatureId + 100
     # ^THIS works, but does it work how I think it works?
     # GenerateCreatures(GenerateCreatures.CreatureId)
--- class1.py
+++ class1.py
@@ -2,95 +2,110 @@ import random
 
 
 class Creature:
-  def __init__(self, type, fed, id):
-    self.type = type
-    self.fed = bool(fed)
-    self.id = int(id)
-    self.position_x = 0
-    self.position_y = 0
-    if type == "Prey":
-        self.speed = 1
-    elif type == "Predator":
-        self.speed = 1  # 2 for later, when I will be able to implement it
+    def __init__(self, creature_type, fed, creature_id):
+        self.creature_type = creature_type
+        self.fed = bool(fed)
+        self.creature_id = int(creature_id)
+        self.position_x = 0
+        self.position_y = 0
+        if creature_type == "Prey":
+            self.speed = 1
+        elif creature_type == "Predator":
+            self.speed = 1  # 2 for later, when I will be able to implement it
+        else:
+            self.speed = 0  # cause plants
 
-  def move(self, direction, map):
-    # 0 north, 1 east, 2 south, 3 west, 4 do not move
-    if direction == 4:
-        return
+    consumables_list = []
 
-    # added modulo, might be an actual smart and cool idea, or something
-    if direction == 0:
-        wanted_y = self.position_y - self.speed
-        wanted_x = self.position_x
-    elif direction == 1:
-        wanted_x = self.position_x - self.speed
-        wanted_y = self.position_y
-    elif direction == 2:
-        wanted_y = self.position_y + self.speed
-        wanted_x = self.position_x
-    elif direction == 3:
-        wanted_x = self.position_x + self.speed
-        wanted_y = self.position_y
+    def consume(self, Map, CreatureList, landing_x, landing_y):
+        raise Exception("Creature has to have a subtype to work")
 
-    wanted_x = wanted_x % map.__len__()
-    wanted_y = wanted_y % map.__len__() 
+    def move(self, direction, Map, CreatureList):
+        # 0 north, 1 east, 2 south, 3 west, 4 do not move
+        if direction == 4:
+            return
 
-    def pick_a_spot(direction):
-        direction_list = []
-        direction_list.append(direction)
-        while True:
-            # select wanted coordinates || REMEMBER !! wanted coordinates DO NOT BECOME actual coordinates !!
-            if direction == 0:
-                wanted_y = self.position_y - self.speed
-                wanted_x = self.position_x
-            elif direction == 1:
-                wanted_x = self.position_x - self.speed
-                wanted_y = self.position_y
-            elif direction == 2:
-                wanted_y = self.position_y + self.speed
-                wanted_x = self.position_x
-            elif direction == 3:
-                wanted_x = self.position_x + self.speed
-                wanted_y = self.position_y
-            
-            wanted_x = wanted_x % map.__len__()
-            wanted_y = wanted_y % map.__len__() 
-            
-            # if on the wanted coordinates there is something
-            if map[wanted_x][wanted_y] is not None:
-                # try all directions (replacement directions)
-                while True:
-                    random_direction = random.randint(0, 3)  # OPTIMISE THIS
-                    # if the new direction is unique
-                    if random_direction not in direction_list:
-                        direction_list.append(random_direction)
-                        direction = direction_list[direction_list.__len__()-1]  # then it becomes the direction
-                        break
-                    # if all possible random numbers were tried, return from pick_a_spot with position_taken
-                    if direction_list.__len__() == 4:
-                        return (True, )
+        def pick_a_spot(direction):
 
-                continue
-            else:
-                return False, direction
+            direction_list = [direction]
+            while True:
+                # select wanted coordinates || REMEMBER !! wanted coordinates DO NOT BECOME actual coordinates !!
+                if direction == 0:
+                    wanted_y = self.position_y - self.speed
+                    wanted_x = self.position_x
+                elif direction == 1:
+                    wanted_x = self.position_x - self.speed
+                    wanted_y = self.position_y
+                elif direction == 2:
+                    wanted_y = self.position_y + self.speed
+                    wanted_x = self.position_x
+                elif direction == 3:
+                    wanted_x = self.position_x + self.speed
+                    wanted_y = self.position_y
+                else:
+                    raise ValueError("No such direction")
 
-    position_taken = pick_a_spot(direction)
+                wanted_x = wanted_x % Map.__len__()
+                wanted_y = wanted_y % Map.__len__()
 
-    if not position_taken[0]:
-        if position_taken[1] == 0:
-            self.position_y = self.position_y - self.speed
-        elif position_taken[1] == 1:
-            self.position_x = self.position_x - self.speed
-        elif position_taken[1] == 2:
-            self.position_y = self.position_y + self.speed
-        elif position_taken[1] == 3:
-            self.position_x = self.position_x + self.speed
-    
-    self.position_x = self.position_x % map.__len__()
-    self.position_y = self.position_y % map.__len__()
+                # if on the wanted coordinates there is something
+                if Map[wanted_x][wanted_y] is not None:
+                    position_freed = self.consume(Map, CreatureList, wanted_x, wanted_y)
+                    # try all directions (replacement directions)
+                    if not position_freed:
+                        while True:
+                            random_direction = random.randint(0, 3)  # OPTIMISE THIS
+                            # if the new direction is unique
+                            if random_direction not in direction_list:
+                                direction_list.append(random_direction)
+                                direction = direction_list[direction_list.__len__() - 1]  # then it becomes the direction
+                                break
+                            # if all possible random numbers were tried, return from pick_a_spot with position_taken
+                            if direction_list.__len__() == 4:
+                                return (True,)
 
-# class Prey(Creature):
-#  def():
+                    continue  # comprehend this continue, when and what purpose it fulfills
+                else:
+                    return False, direction
+
+        position_taken = pick_a_spot(direction)
+
+        if not position_taken[0]:
+            if position_taken[1] == 0:
+                self.position_y = self.position_y - self.speed
+            elif position_taken[1] == 1:
+                self.position_x = self.position_x - self.speed
+            elif position_taken[1] == 2:
+                self.position_y = self.position_y + self.speed
+            elif position_taken[1] == 3:
+                self.position_x = self.position_x + self.speed
+
+        self.position_x = self.position_x % Map.__len__()
+        self.position_y = self.position_y % Map.__len__()
 
 
-# class Predator(Creature):
+class Pray(Creature):
+    consumables_list = ["Plant"]
+
+    def consume(self, Map, CreatureList, landing_x, landing_y):
+        #  if Map[landing_x][landing_y] is not None:  I don't think this check is necessary
+        if Map[landing_x][landing_y].creature_type in Pray.consumables_list:
+            # del CreatureList[CreatureList.index(Map[landing_x][landing_y])] overcomplicated and dumb
+            CreatureList.remove(Map[landing_x][landing_y])  # <-- PROBABLE CAUSE OF THE PROBLEM
+
+
+class Predator(Creature):
+    consumables_list = ["Pray"]
+
+    def consume(self, Map, CreatureList, landing_x, landing_y):
+        if Map[landing_x][landing_y].creature_type in Predator.consumables_list:
+            CreatureList.remove(Map[landing_x][landing_y])
+            return True
+        else:
+            return False
+
+
+class Plant(Creature):  # Bear with me
+    # this should be a 'vegetable' aka exist, do nothing and not decide to stop existing by itself
+    def move(self, direction, Map, CreatureList):
+        raise Exception("A Plant cannot move")
